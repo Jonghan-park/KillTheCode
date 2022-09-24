88c2a9e1a11b4a6c358b60811872f5a9eff74a90
Jonghan Park <jonghan.park@edu.sait.ca>
2022-09-24 08:55:52 -0600

    Update left and right message on main page

--- frontend/src/components/Main/Main.js
+++ frontend/src/components/Main/Main.js
@@ -1,18 +1,24 @@
 import React from "react";
+import leftIcon from "../../assets/leftMessage.png";
+import rightIcon from "../../assets/rightMessage.png";
 import "./Main.css";
 
 const Main = () => {
   return (
     <main className="mainPage">
       <div className="mainContainer">
-        <div className="leftIcon"></div>
+        <div className="leftIcon">
+          <img src={leftIcon} alt="Left message" />
+        </div>
         <div className="mainMessage">
           <p>
             We are here to <span>create project</span> and keep developing
             <span> something creative.</span>
           </p>
         </div>
-        <div className="rightIcon"></div>
+        <div className="rightIcon">
+          <img src={rightIcon} alt="Right message" />
+        </div>
       </div>
     </main>
   );
