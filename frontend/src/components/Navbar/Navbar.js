785494435ee45ce8b189d9b28bdc460640de7063
Jonghan Park <jonghan.park@edu.sait.ca>
2022-09-24 08:00:11 -0600

    Set main links and signin links, and import 'Cabin' fonts

--- frontend/src/components/Navbar/Navbar.js
+++ frontend/src/components/Navbar/Navbar.js
@@ -1,29 +1,37 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import logo from "../../assets/logo.png";
+import "./Navbar.css";
 
 const Navbar = () => {
   return (
     <nav className="navbar">
-      <div className="logo">
-        <Link to="/">
-          <img src={logo} alt="logo" />
-        </Link>
+      <div className="logoAndMainLinks">
+        <div className="logo">
+          <Link to="/">
+            <img src={logo} alt="logo" />
+          </Link>
+        </div>
+        <div className="mainLinks">
+          <div className="navbarLink">
+            <Link to="/about">About</Link>
+          </div>
+          <div className="navbarLink">
+            <Link to="/notice">Notice</Link>
+          </div>
+          <div className="navbarLink">
+            <Link to="/projects">Projects</Link>
+          </div>
+        </div>
       </div>
-      <ul className="navbar-links">
-        <li className="navbar-item">
-          <Link to="/">Home</Link>
-        </li>
-        <li className="navbar-item">
-          <Link to="/menu">Menu</Link>
-        </li>
-        <li className="navbar-item">
-          <Link to="/about">AboutUs</Link>
-        </li>
-        <li className="navbar-item">
-          <Link to="/contact">Contact</Link>
-        </li>
-      </ul>
+      <div className="signInLinks">
+        <div className="navbarLink">
+          <Link to="/signin">Sign In</Link>
+        </div>
+        <div className="navbarLink">
+          <Link to="/joinus">Join Us</Link>
+        </div>
+      </div>
     </nav>
   );
 };
