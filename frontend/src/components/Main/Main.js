517c3b1b81f493289bb6de97c5c3f33dc038e05f
Jonghan Park <jonghan.park@edu.sait.ca>
2022-09-24 08:35:45 -0600

    Update main page

--- frontend/src/components/Main/Main.js
+++ frontend/src/components/Main/Main.js
@@ -1,7 +1,21 @@
 import React from "react";
+import "./Main.css";
 
 const Main = () => {
-  return <div>Main</div>;
+  return (
+    <main className="mainPage">
+      <div className="mainContainer">
+        <div className="leftIcon"></div>
+        <div className="mainMessage">
+          <p>
+            We are here to <span>create project</span> and keep developing
+            <span> something creative.</span>
+          </p>
+        </div>
+        <div className="rightIcon"></div>
+      </div>
+    </main>
+  );
 };
 
 export default Main;
