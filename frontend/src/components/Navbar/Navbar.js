2815372e01952f1f3f71ad5bf6e3a633f5cad27f
Jonghan Park <jonghan.park@edu.sait.ca>
2022-09-24 07:21:22 -0600

    Import logo, and initialize Navbar

--- frontend/src/components/Navbar/Navbar.js
+++ frontend/src/components/Navbar/Navbar.js
@@ -1,7 +1,31 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import logo from "../../assets/logo.png";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <nav className="navbar">
+      <div className="logo">
+        <Link to="/">
+          <img src={logo} alt="logo" />
+        </Link>
+      </div>
+      <ul className="navbar-links">
+        <li className="navbar-item">
+          <Link to="/">Home</Link>
+        </li>
+        <li className="navbar-item">
+          <Link to="/menu">Menu</Link>
+        </li>
+        <li className="navbar-item">
+          <Link to="/about">AboutUs</Link>
+        </li>
+        <li className="navbar-item">
+          <Link to="/contact">Contact</Link>
+        </li>
+      </ul>
+    </nav>
+  );
 };
 
 export default Navbar;
