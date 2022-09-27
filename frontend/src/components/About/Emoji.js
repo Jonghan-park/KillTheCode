e43b5be9fb30b8bc90860e692a547012e4e1b97d
Jonghan Park <jonghan.park@edu.sait.ca>
2022-09-26 19:44:28 -0600

    Update navigation of icons in main page

--- frontend/src/components/About/Emoji.js
+++ frontend/src/components/About/Emoji.js
@@ -2,10 +2,17 @@ import React from "react";
 import "./Emoji.css";
 
 const Emoji = ({ emojis }) => {
+  const emojiHandler = (index) => {
+    console.log(index + 1);
+  };
   return (
     <div className="EmojiContainer">
-      {emojis.map((emoji) => {
-        return <div className="Emoji">{emoji}</div>;
+      {emojis.map((emoji, index) => {
+        return (
+          <div className="Emoji" onClick={() => emojiHandler(index)}>
+            {emoji}
+          </div>
+        );
       })}
     </div>
   );
