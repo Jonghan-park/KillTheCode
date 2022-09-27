c2407408709fe1562281197a93c0eba928121338
Jonghan Park <jonghan.park@edu.sait.ca>
2022-09-26 18:12:17 -0600

    Create Emoji component, and align items in about.js

--- frontend/src/components/About/About.js
+++ frontend/src/components/About/About.js
@@ -1,10 +1,25 @@
 import React from "react";
+import Emoji from "./Emoji";
 import "./About.css";
 
 const About = () => {
   return (
     <div className="aboutContainer">
-      <h1>This is About</h1>
+      <div className="aboutTitle">
+        <p>
+          <span className="highlight">Let</span> We{" "}
+          <span className="highlight">Introduce</span>
+        </p>
+      </div>
+      <div className="aboutDetail">
+        <p>
+          Team Kill The Code was organized in June, 2022 and started our first
+          team project in September, 2022. We{" "}
+          <span className="highlight">create</span> our team project with{" "}
+          <span className="highlight">innovative ideas.</span>{" "}
+        </p>
+      </div>
+      <Emoji />
     </div>
   );
 };
---
+++ frontend/src/components/About/Emoji.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Emoji.css";
+
+const Emoji = () => {
+  return <div>Emoji</div>;
+};
+
+export default Emoji;
