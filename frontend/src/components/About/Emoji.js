5edff9835930f144239d7118817615f42eb4e551
Jonghan Park <jonghan.park@edu.sait.ca>
2022-09-28 16:39:08 -0600

    Update modal

--- frontend/src/components/About/Emoji.js
+++ frontend/src/components/About/Emoji.js
@@ -1,20 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
+import Modal from "../AboutModal/Modal";
 import "./Emoji.css";
 
 const Emoji = ({ users }) => {
-  const emojiHandler = (index) => {
-    console.log(users[index]);
+  const [show, setShow] = useState(false);
+  const [curIndex, setCurIndex] = useState(0);
+
+  const openModal = (index) => {
+    setCurIndex(index);
+    setShow((prev) => !prev);
   };
   return (
     <div className="emojiContainer">
       {users.map((user, index) => {
-        const { name, skill, linkedIn, github, email, emojiInUser } = user;
         return (
-          <div className="emoji" onClick={() => emojiHandler(index)}>
-            {emojiInUser}
+          <div key={index} className="emoji" onClick={() => openModal(index)}>
+            {user.emojiInUser}
           </div>
         );
       })}
+      <Modal user={users[curIndex]} closeModal={show} setCloseModal={setShow} />
     </div>
   );
 };
--- frontend/src/components/AboutModal/Modal.js
+++ frontend/src/components/AboutModal/Modal.js
@@ -1,8 +1,35 @@
 import React from "react";
-import "Modal.css";
+import "./Modal.css";
 
-const Modal = () => {
-  return <div>Modal</div>;
+const Modal = ({ user, closeModal, setCloseModal }) => {
+  console.log(closeModal);
+  return (
+    <>
+      {closeModal ? (
+        <div className="modalContainer">
+          <div className="modalContent">
+            <div className="modalHeader">Modal title</div>
+            <div className="modalBody">
+              <p>{user.emojiInUser}</p>
+              <h3>Hi, my name is {user.name}</h3>
+              <p>Skill: {user.skill}</p>
+              <p>linkedIn: {user.linkedIn}</p>
+              <p>Github: {user.github}</p>
+              <p>Email: {user.email}</p>
+            </div>
+            <div className="modalFooter">
+              <button
+                className="modalButton"
+                onClick={() => setCloseModal(false)}
+              >
+                Close
+              </button>
+            </div>
+          </div>
+        </div>
+      ) : null}
+    </>
+  );
 };
 
 export default Modal;
