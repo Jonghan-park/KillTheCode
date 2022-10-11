79d21881bd5f61f92e3dc0ca9f2e9a939aa2a825
Lauren <lauren.sung23@gmail.com>
2022-10-10 22:13:35 -0600

    working on chatting / create text box

--- frontend/src/components/Chatting/Chatting.js
+++ frontend/src/components/Chatting/Chatting.js
@@ -1,9 +1,48 @@
 import "./Chatting.css";
 
-const Chatting = () => {
+const messageEx = [
+  {
+    message: "hello",
+    username: "L"
+  }
+];
+
+
+
+function Chatting({socket, username}){
+  // const [currentMessage, setCurrentMessage] = useState("");
+
+  // const sendMessage = async() =>{
+  //   if(currentMessage !== "") {
+  //      const messageData = {
+  //       username: username,
+  //       message: currentMessage,
+  //      }
+  //   }
+  // }
+
+
   return (
     <div className="chattingContainer">
-      Chatting!!
+      <div className="chat-header"></div>
+
+      <div className="chat-body">
+
+
+      </div>
+
+      <div className="chat-footer">
+        <input 
+        type="text" 
+        placeholder="enter text..."
+        onChange={(event)=>{
+          // setCurrentMessage(event.target.value)
+        }}
+        />
+      <button>&#9658;</button>
+      </div>
+      
+  
     </div>
   );
 };
