7b595eab4307281a84c08e1f43fe2ad7dd474e84
KaiLee85 <87049080+KaiLee85@users.noreply.github.com>
2022-10-10 20:31:11 -0600

    before fix radio btn bug

--- frontend/src/components/Admin/users.js
+++ frontend/src/components/Admin/users.js
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import "./admin.css";
 
 const UserList = ({ users }) => {
+  // const [userCooperate, setCooperate] = useState;
   return (
     <>
       <table className="userTable">
