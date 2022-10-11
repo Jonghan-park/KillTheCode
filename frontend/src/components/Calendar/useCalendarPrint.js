65b3e7155afdedde9d5393329065c932fd75bce5
Eunji-Elly-Lee <eunji.lee@edu.sait.ca>
2022-10-11 15:56:00 -0600

    Create useCalendarPrint hook

---
+++ frontend/src/components/Calendar/useCalendarPrint.js
@@ -0,0 +1,115 @@
+import { useMemo } from "react";
+
+const useCalendarPrint = ({
+  weeks,
+  currentYear,
+  currentMonth,
+  schedules,
+  meetings,
+}) => {
+  return useMemo(() => {
+    const lastDate = new Date(currentYear, currentMonth, 0).getDate();
+    const firstDay = new Date(currentYear, currentMonth - 1, 1).getDay();
+    const lastDay = new Date(currentYear, currentMonth - 1, lastDate).getDay();    
+    let calendarCells = [];
+
+    for (const day of weeks) {
+      if (weeks[firstDay] === day) {
+        for (let i = 1; i <= lastDate; i++) {
+          const nowDay = new Date(currentYear, currentMonth - 1, i).getDay();
+          let scheduled = false;
+
+          for (const schedule of schedules) {
+            const fromDay = 
+              new Date(currentYear, currentMonth - 1, schedule.fromDate).getDay();
+
+            if (schedule.fromYear === currentYear &&
+                schedule.fromMonth === currentMonth &&
+                schedule.toMonth === schedule.fromMonth &&
+                (i >= schedule.fromDate && i <= schedule.toDate)) {
+              scheduled = true;
+            } else if (schedule.fromYear === currentYear &&
+                schedule.fromMonth === currentMonth &&
+                schedule.toMonth !== schedule.fromMonth &&
+                (i >= schedule.fromDate && i <= lastDate)) {
+              scheduled = true;
+            } else if (schedule.fromYear === currentYear &&
+                schedule.toMonth === currentMonth &&
+                schedule.toMonth !== schedule.fromMonth &&
+                (i >= 1 && i <= schedule.toDate)) {
+              scheduled = true;
+            } else if (schedule.toYear === currentYear &&
+                schedule.toMonth === currentMonth &&
+                schedule.toMonth !== schedule.fromMonth &&
+                (i >= 1 && i <= schedule.toDate)) {
+              scheduled = true;
+            }
+
+            if (scheduled) {
+              const issued =
+                (schedule.fromDate === i && fromDay !== 6) ||
+                (fromDay === 6 && nowDay === 0);
+              let met = false;
+              let attendees = "";
+
+              for (const meeting of meetings) {
+                if (meeting.meetingYear === currentYear &&
+                    meeting.meetingMonth === currentMonth &&
+                    meeting.meetingDate === i) {
+                  met = true;
+                  attendees = meeting.attendees;
+                }
+              }
+
+              calendarCells.push(
+                <div key={i} className="calendarCell">
+                  {i}
+                  <div
+                    className={
+                      "schedule" +
+                      (schedule.fromDate === i ? " startSchedule" : "") +
+                      (schedule.toDate === i ? " endSchedule" : "") +
+                      (issued ? " issue" : "")
+                    }
+                    style={{
+                      backgroundColor: schedule.color,
+                    }}
+                  >
+                    {issued && <span>{schedule.issue}</span>}
+                  </div>
+                  {met && <div className="meeting">Meeting</div>}
+                </div>
+              );
+
+              break;
+            }
+          }
+
+          if (!scheduled) {
+            calendarCells.push(
+              <div key={i} className="calendarCell">{i}</div>
+            );
+          }
+        }
+
+        break;
+      } else {
+        calendarCells.push(
+          <div key={day} className="calendarCell"></div>
+        );
+      }
+    }
+
+    if (lastDay !== 6) {
+      for (let i = lastDay + 1; i <= 6; i++) {
+        calendarCells.push(
+          <div key={i * 100} className="calendarCell"></div>
+        );
+      }
+    }
+
+    return calendarCells;
+  }, [weeks, currentYear, currentMonth, schedules, meetings]);
+};
+
+export default useCalendarPrint;
