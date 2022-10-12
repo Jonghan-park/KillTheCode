d8ec0364293e0a2940274f1e66f1334bd4ba4bf9
Eunji-Elly-Lee <eunji.lee@edu.sait.ca>
2022-10-12 09:29:41 -0600

    Update meeting modal

--- frontend/src/components/MeetingModal/MeetingModal.js
+++ frontend/src/components/MeetingModal/MeetingModal.js
@@ -1,12 +1,25 @@
+import { useState } from "react";
 import "./MeetingModal.css";
 
 const MeetingModal = ({
-  dateInfo,
-  attendees,
+  initMeeting,
+  meetingInfo,
   setShowMeetingModal,
+  setMeetingInfo,
 }) => {
+  const [editing, setEditing] =
+    useState(meetingInfo.dateInfo === "" ? false : true);
+
   const closeMeetingModal = () => {
     setShowMeetingModal(false);
+    setMeetingInfo(initMeeting);
+  };
+  const onMeetingChange = (e) => {
+    const { name, value } = e.target;
+    setMeetingInfo({
+      ...meetingInfo,
+      [name]: value,
+    })
   };
   return ( 
     <div className="modalBackground" onClick={closeMeetingModal}>
@@ -15,10 +28,39 @@ const MeetingModal = ({
           e.stopPropagation();
         }}
       >
-        !!MeetingModal!!
-        <button onClick={closeMeetingModal}>
-          Close
-        </button>
+        <form>
+          <label htmlFor="dateInfo">Date</label>
+          <input
+            type="date"
+            required
+            name="dateInfo"
+            id="dateInfo"
+            value={meetingInfo.dateInfo}
+            onChange={onMeetingChange}
+          />
+          <label htmlFor="attendees">Attendees</label>
+          <input
+            type="text"
+            required
+            name="attendees"
+            id="attendees"
+            value={meetingInfo.attendees}
+            onChange={onMeetingChange}
+          />
+          <div className="modalBtns">
+            {editing &&
+              <button className="modalLeftBtn">
+                Delete
+              </button>            
+            }
+            <button className="modalRightBtn" onClick={closeMeetingModal}>
+              Close
+            </button>
+            <button type="submit" className="modalRightBtn">
+              {editing ? "Update" : "Submit"}
+            </button>
+          </div>
+        </form>
       </div>
     </div>
   );
