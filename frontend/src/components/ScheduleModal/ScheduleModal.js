ce39e1295ef336d834e9a70559dd38b8f4b38f3e
Eunji-Elly-Lee <eunji.lee@edu.sait.ca>
2022-10-12 09:29:30 -0600

    Update schedule modal

--- frontend/src/components/ScheduleModal/ScheduleModal.js
+++ frontend/src/components/ScheduleModal/ScheduleModal.js
@@ -1,14 +1,25 @@
+import { useState } from "react";
 import "./ScheduleModal.css";
 
 const ScheduleModal = ({
-  fromDateInfo,
-  toDateInfo,
-  issue,
-  color,
+  initSchedule,
+  scheduleInfo,
   setShowScheduleModal,
+  setScheduleInfo,
 }) => {
+  const [editing, setEditing] =
+    useState(scheduleInfo.fromDateInfo === "" ? false : true);
+
   const closeScheduleModal = () => {
     setShowScheduleModal(false);
+    setScheduleInfo(initSchedule);
+  };
+  const onScheduleChange = (e) => {
+    const { name, value } = e.target;
+    setScheduleInfo({
+      ...scheduleInfo,
+      [name]: value,
+    })
   };
   return ( 
     <div className="modalBackground" onClick={closeScheduleModal}>
@@ -17,10 +28,56 @@ const ScheduleModal = ({
           e.stopPropagation();
         }}
       >
-        !!ScheduleModal!!
-        <button onClick={closeScheduleModal}>
-          Close
-        </button>
+        <form>
+          <label htmlFor="fromDateInfo">From</label>
+          <input
+            type="date"
+            required
+            name="fromDateInfo"
+            id="fromDateInfo"
+            value={scheduleInfo.fromDateInfo}
+            onChange={onScheduleChange}
+          />
+          <label htmlFor="toDateInfo">To</label>
+          <input
+            type="date"
+            required
+            name="toDateInfo"
+            id="toDateInfo"
+            value={scheduleInfo.toDateInfo}
+            onChange={onScheduleChange}
+          />
+          <label htmlFor="issue">Issue</label>
+          <input
+            type="text"
+            required
+            name="issue"
+            id="issue"
+            value={scheduleInfo.issue}
+            onChange={onScheduleChange}
+          />
+          <label htmlFor="color">Color</label>
+          <input
+            type="color"
+            name="color"
+            id="color"
+            value={scheduleInfo.color}
+            onChange={onScheduleChange}
+          />
+          <div className="modalBtns">
+            {editing &&
+              <button className="modalLeftBtn">
+                Delete
+              </button>            
+            }
+            <button className="modalRightBtn" onClick={closeScheduleModal}>
+              Close
+            </button>
+            <button type="submit" className="modalRightBtn">
+              {editing ? "Update" : "Submit"}
+            </button>
+          </div>
+        </form>
       </div>
     </div>
   );
