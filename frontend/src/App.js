d24990729b48e72b1bf5a565f400daa3ae53f7a4
Eunji-Elly-Lee <eunji.lee@edu.sait.ca>
2022-10-12 09:34:52 -0600

    Update App for notice

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import Navbar from "./components/Navbar/Navbar";
 import Main from "./components/Main/Main";
 import About from "./components/About/About";
 import Projects from "./components/Projects/Projects";
+import Notice from "./components/Notice/Notice";
 import Footer from "./components/Footer/Footer";
 import SignIn from "./components/SignIn/SignIn";
 import JoinUs from "./components/JoinUs/JoinUs";
@@ -20,6 +21,7 @@ function App() {
           <Route path="/" element={<Main />} />
           <Route path="/about" element={<About />} />
           <Route path="/projects" element={<Projects />} />
+          <Route path="/notice" element={<Notice />} />
           <Route path="/myaccount" element={<Myaccount />} />
           <Route path="/signin" element={<SignIn />} />
           <Route path="/joinus" element={<JoinUs />} />
