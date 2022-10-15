de80c7a4be24659fb08bd37ec9529aca523990e2
Eunji-Elly-Lee <eunji.lee@edu.sait.ca>
2022-10-15 02:12:12 -0600

    Update App for notice view by id

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,6 +7,7 @@ import Main from "./components/Main/Main";
 import About from "./components/About/About";
 import Projects from "./components/Projects/Projects";
 import Notice from "./components/Notice/Notice";
+import NoticeView from "./components/Notice/NoticeView";
 import Footer from "./components/Footer/Footer";
 import SignIn from "./components/SignIn/SignIn";
 import JoinUs from "./components/JoinUs/JoinUs";
@@ -29,6 +30,7 @@ function App() {
             <Route path="/about" element={<About />} />
             <Route path="/projects" element={<Projects />} />
             <Route path="/notice" element={<Notice />} />
+            <Route path="/notice/:id" element={<NoticeView />} />
             <Route path="/myaccount" element={<Myaccount />} />
             <Route path="/signin" element={<SignIn />} />
             <Route path="/joinus" element={<JoinUs />} />
