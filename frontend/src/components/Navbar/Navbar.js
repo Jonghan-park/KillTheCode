0891116d96e4be45b92a8bb72189beadd155ec4a
Lauren <lauren.sung23@gmail.com>
2022-10-21 23:27:23 -0600

    add path myaccount

--- frontend/src/components/Navbar/Navbar.js
+++ frontend/src/components/Navbar/Navbar.js
@@ -24,7 +24,7 @@ const Navbar = () => {
 
   useEffect(() => {
     console.log(window.location.pathname);
-    if (window.location.pathname === "/admin") {
+    if (window.location.pathname === "/admin" || "/myaccount") {
       setLightTheme(true);
     } else {
       setLightTheme(false);
