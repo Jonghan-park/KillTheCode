651038e9a4627e0cf55bb5735a94999b6ef4747f
KaiLee85 <87049080+KaiLee85@users.noreply.github.com>
2022-10-22 09:00:34 -0600

    Delete myaccount on navbar

--- frontend/src/components/Navbar/Navbar.js
+++ frontend/src/components/Navbar/Navbar.js
@@ -24,7 +24,7 @@ const Navbar = () => {
 
   useEffect(() => {
     console.log(window.location.pathname);
-    if (window.location.pathname === "/admin" || "/myaccount") {
+    if (window.location.pathname === "/admin") {
       setLightTheme(true);
     } else {
       setLightTheme(false);
