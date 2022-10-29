6d58a9308469a499eed12242098a6308b8b43343
Jonghan Park <jonghan.park@edu.sait.ca>
2022-10-29 14:37:10 -0600

    Set up database

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,15 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
 require("dotenv").config();
+const cors = require("cors");
+
+// DB
+const connection = require("./database/db");
+
+// Database connection
+connection();
+
+app.use(cors());
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
