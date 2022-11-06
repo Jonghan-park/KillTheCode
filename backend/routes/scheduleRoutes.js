abb45920001621ca5154e47bf6660cbb0c5a70f1
Lauren <lauren.sung23@gmail.com>
2022-11-05 23:16:31 -0600

    create CRUD  schedule controller and routes

--- backend/routes/scheduleRoutes.js
+++ backend/routes/scheduleRoutes.js
@@ -2,7 +2,15 @@ const express = require("express");
 const scheduleRouter = express.Router();
 
 const{
+    getAllSchedules,
+    addSchedule,
+    updateSchedule,
+
 
 } = require("../controller/scheduleController");
+
+scheduleRouter.get("/", getAllSchedules);
+scheduleRouter.post("/add", addSchedule);
+scheduleRouter.put("/update/:id", updateSchedule);
 
 module.exports = scheduleRouter;
