65062ee2c952d021a1d6d6617ce8f38ad0731fee
KaiLee85 <87049080+KaiLee85@users.noreply.github.com>
2022-11-06 20:41:42 -0700

    get current user data

--- frontend/src/features/auth/authSlice.js
+++ frontend/src/features/auth/authSlice.js
@@ -2,9 +2,10 @@ import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import authService from "./authService";
 
 //get user from local storage
+const user = JSON.parse(localStorage.getItem("user"));
 
 const initialState = {
-  user: null,
+  user: user ? user : null,
   isError: false,
   isSuccess: false,
   isLoading: false,
