9dfae9bcbc84460fbae2144cce30f929d0eb21fd
KaiLee85 <87049080+KaiLee85@users.noreply.github.com>
2022-11-11 08:35:42 -0700

    create myaccount route

--- backend/server.js
+++ backend/server.js
@@ -12,6 +12,7 @@ const chattingRoute = require("./routes/chattingRoutes");
 const scheduleRoute = require("./routes/scheduleRoutes");
 
 const noticeRoute = require("./routes/noticeRouter");
+const myAccountRoute = require("./routes/myaccountRoutes");
 //DB
 const connection = require("./database/db");
 
@@ -29,7 +30,7 @@ app.use("/meeting", meetingRoute);
 app.use("/chatting", chattingRoute);
 app.use("/schedule", scheduleRoute);
 app.use("/notice", noticeRoute);
-
+app.use("/myaccount", myAccountRoute);
 const server = app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
