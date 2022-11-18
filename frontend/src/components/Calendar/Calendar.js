e125793a4ad944eb73a2e74fe1f696167972c0cd
Lauren <lauren.sung23@gmail.com>
2022-11-17 21:12:42 -0700

    add post and get meeting & schedule

--- frontend/src/components/Calendar/Calendar.js
+++ frontend/src/components/Calendar/Calendar.js
@@ -1,90 +1,10 @@
-import { useCallback, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import useCalendarPrint from "./useCalendarPrint";
 import ScheduleModal from "../ScheduleModal/ScheduleModal";
 import MeetingModal from "../MeetingModal/MeetingModal";
 import "./Calendar.css";
-
-const schedule = [
-  {
-    // from: "2022-09-05",
-    // to: "2022-09-08",
-    fromYear: 2022,
-    fromMonth: 9,
-    fromDate: 5,
-    toYear: 2022,
-    toMonth: 9,
-    toDate: 8,
-    issue: "Plan Project",
-    color: "#EE7098",
-  },
-  {
-    fromYear: 2022,
-    fromMonth: 9,
-    fromDate: 9,
-    toYear: 2022,
-    toMonth: 9,
-    toDate: 13,
-    issue: "Sketch Design",
-    color: "#545BE3",
-  },
-  {
-    fromYear: 2022,
-    fromMonth: 9,
-    fromDate: 14,
-    toYear: 2022,
-    toMonth: 9,
-    toDate: 22,
-    issue: "Develope Design",
-    color: "#8932E3",
-  },
-  {
-    fromYear: 2022,
-    fromMonth: 9,
-    fromDate: 23,
-    toYear: 2022,
-    toMonth: 10,
-    toDate: 14,
-    issue: "Coding Frontend",
-    color: "#50B027",
-  },
-  {
-    fromYear: 2022,
-    fromMonth: 10,
-    fromDate: 15,
-    toYear: 2022,
-    toMonth: 10,
-    toDate: 19,
-    issue: "Develop Frontend",
-    color: "#1CCAED",
-  },
-];
-const meeting = [
-  {
-    // date: "2022-09-08",
-    meetingYear: 2022,
-    meetingMonth: 9,
-    meetingDate: 8,
-    attendees: "Kai, Lauren, Elly",
-  },
-  {
-    meetingYear: 2022,
-    meetingMonth: 9,
-    meetingDate: 14,
-    attendees: "Joseph, Kai, Lauren, Elly",
-  },
-  {
-    meetingYear: 2022,
-    meetingMonth: 9,
-    meetingDate: 23,
-    attendees: "Joseph, Kai, Lauren, Elly",
-  },
-  {
-    meetingYear: 2022,
-    meetingMonth: 10,
-    meetingDate: 15,
-    attendees: "Joseph, Kai, Lauren, Elly, Jihoon",
-  },
-];
+import axios from "axios";
+import {useNavigate, useParams} from "react-router-dom";
 
 const Calendar = ({projectId}) => {
   const today = {
@@ -106,8 +26,8 @@ const Calendar = ({projectId}) => {
   };
   const [currentYear, setCurrentYear] = useState(today.year);
   const [currentMonth, setCurrentMonth] = useState(today.month);
-  const [schedules, setSchedules] = useState(schedule);
-  const [meetings, setMeetings] = useState(meeting);
+  const [schedules, setSchedules] = useState([]);
+  const [meetings, setMeetings] = useState([]);
   const [showScheduleModal, setShowScheduleModal] = useState(false);
   const [showMeetingModal, setShowMeetingModal] = useState(false);
   const [scheduleInfo, setScheduleInfo] = useState(initSchedule);
@@ -150,6 +70,47 @@ const Calendar = ({projectId}) => {
       setCurrentMonth(currentMonth + 1);
     }
   }, [currentMonth, currentYear]);
+
+
+// ---
+// meeting
+
+  useEffect(()=>{
+    const getMeeting = async () => { 
+      //all
+     const res = await axios.get(`http://localhost:5000/meeting/${projectId}`)
+     .catch(err => console.log(err))
+     const data =await res.data;
+    //  console.log(data);  
+     return data;
+   }
+    if(projectId){
+      getMeeting()
+      .then((data) => setMeetings(data))
+    }
+  },[projectId]);
+  // console.log(meetings)
+
+
+// ---schedule
+useEffect(()=>{
+  const getSchedule = async () => { 
+    //all
+   const res = await axios.get(`http://localhost:5000/schedule/${projectId}`)
+   .catch(err => console.log(err))
+   
+   const data =await res.data;
+  //  console.log(data);   
+   return data;
+ }
+  if(projectId){
+    getSchedule()
+    .then((data) => setSchedules(data))
+  }
+  console.log(schedules)
+},[projectId]);
+  // console.log(schedules)
+
   return (
     <div className="calendarContainer">
       <div className="calendarHeader">
